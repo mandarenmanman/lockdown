f6b00829602529d691c3169a37a261b715e55381
mandaren <1793575354@qq.com>
2022-10-11 11:19:36 +0800

    态势图修改

--- datav/js/mymap.js
+++ datav/js/mymap.js
@@ -1,8 +1,9 @@
 /*大屏*/
 var geoCoordMap = {
-    '上海市': [121.461801,31.253098],
-    '泉州市': [118.683594,24.888081],
-    '乌鲁木齐市': [87.60949,43.816819],
+    '上海市': [121.461801, 31.253098],
+    '泉州市': [118.683594, 24.888081],
+    '乌鲁木齐市': [87.60949, 43.816819],
+    '西安市':[108.937266, 34.350369],
 
 };
 
@@ -14,7 +15,13 @@ var lockdownData = [
         name: '乌鲁木齐市',
         value: 200
     }],
-  
+    [{
+        name: '西安市'
+    }, {
+        name: '西安市',
+        value: 200
+    }],
+
 ];
 
 var historyData = [
@@ -30,14 +37,14 @@ var historyData = [
         name: '泉州市',
         value: 100
     }],
-  
+
 ];
 
 
 
 var planePath = 'path://M.6,1318.313v-89.254l-319.9-221.799l0.073-208.063c0.521-84.662-26.629-121.796-63.961-121.491c-37.332-0.305-64.482,36.829-63.961,121.491l0.073,208.063l-319.9,221.799v89.254l330.343-157.288l12.238,241.308l-134.449,92.931l0.531,42.034l175.125-42.917l175.125,42.917l0.531-42.034l-134.449-92.931l12.238-241.308L1705';
 
-var convertData = function (data) {
+var convertData = function(data) {
     var res = [];
     for (var i = 0; i < data.length; i++) {
         var dataItem = data[i];
@@ -57,9 +64,9 @@ var convertData = function (data) {
 var color = ['#ffa022', '#a6c84c'];
 var series = [];
 [
-    ['封神榜', lockdownData],//
-    ['history', historyData],//
-].forEach(function (item, i) {
+    ['封神榜', lockdownData], //
+    ['history', historyData], //
+].forEach(function(item, i) {
     series.push({
         name: item[0] + ' Top10',
         type: 'lines',
@@ -114,7 +121,7 @@ var series = [];
                 formatter: '{b}'
             }
         },
-        symbolSize: function (val) {
+        symbolSize: function(val) {
             return val[2] / 8;
         },
         itemStyle: {
@@ -122,7 +129,7 @@ var series = [];
                 color: color[i]
             }
         },
-        data: item[1].map(function (dataItem) {
+        data: item[1].map(function(dataItem) {
             return {
                 name: dataItem[1].name,
                 value: geoCoordMap[dataItem[1].name].concat([dataItem[1].value])
@@ -155,7 +162,7 @@ option = {
     geo: {
         map: 'china',
         zoom: 0.8,
-        center: [104.961438,33.701706],
+        center: [104.961438, 33.701706],
         label: {
             emphasis: {
                 show: false
